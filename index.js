1b626dc4a543ea0a1cd9fa8d5f91c3a84177c159
rysian2 <ryisan2@gmail.com>
2024-04-08 08:33:54 -0400

    changes 7am

--- index.js
+++ index.js
@@ -1,5 +1,36 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-    const fourthrow = document.querySelector('.fourthrow');
+    const features= document.querySelector('.features',);
+    const windowHeight = window.innerHeight;
+
+    window.addEventListener('scroll', () => {
+        // Get the position of the .features section
+        const scrollY = window.scrollY || window.pageYOffset;
+        const featuresTop = features.getBoundingClientRect().top + scrollY;
+        
+        // Check if the .features section has scrolled into view
+        if (scrollY + windowHeight > featuresTop) {
+            features.classList.add('features-visible');
+        }
+    });
+});
+document.addEventListener('DOMContentLoaded', (event) => {
+    const services= document.querySelector('.services',);
+    const windowHeight = window.innerHeight;
+
+    window.addEventListener('scroll', () => {
+        // Get the position of the .services section
+        const scrollY = window.scrollY || window.pageYOffset;
+        const servicesTop = services.getBoundingClientRect().top + scrollY;
+        
+        // Check if the .services section has scrolled into view
+        if (scrollY + windowHeight > servicesTop) {
+            services.classList.add('services-visible');
+        }
+    });
+});
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const fourthrow = document.querySelector('.fourthrow',);
     const windowHeight = window.innerHeight;
 
     window.addEventListener('scroll', () => {
@@ -10,6 +41,22 @@ document.addEventListener('DOMContentLoaded', (event) => {
         // Check if the .fourthrow section has scrolled into view
         if (scrollY + windowHeight > fourthrowTop) {
             fourthrow.classList.add('fourthrow-visible');
+        }
+    });
+});
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const fifthrow= document.querySelector('.fifthrow',);
+    const windowHeight = window.innerHeight;
+
+    window.addEventListener('scroll', () => {
+        // Get the position of the .fifthrow section
+        const scrollY = window.scrollY || window.pageYOffset;
+        const fifthrowTop = fifthrow.getBoundingClientRect().top + scrollY;
+        
+        // Check if the .fifthrow section has scrolled into view
+        if (scrollY + windowHeight > fifthrowTop) {
+            fifthrow.classList.add('fifthrow-visible');
         }
     });
 });
